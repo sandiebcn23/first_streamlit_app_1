1e86d8ed35f775491aed0f73977390f8dcb78997
sandiebcn23 <126023504+sandiebcn23@users.noreply.github.com>
2023-02-21 22:32:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title ('My parents new healthy diner');
+
+streamlit.header ('Breakfast Menu');
+streamlit.text('Oatmeal');
+streamlit.text('Smoothie');
+streamlit.text('Hard Boiled Egg');
