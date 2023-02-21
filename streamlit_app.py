8b7e28c8e9b038280caa6a106431d6ecb735116d
sandiebcn23 <126023504+sandiebcn23@users.noreply.github.com>
2023-02-21 22:33:10 +0100

    Update streamlit_app.py
    fixed typo in the last version

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ streamlit.title ('My parents new healthy diner');
 streamlit.header ('Breakfast Menu');
 streamlit.text('Oatmeal');
 streamlit.text('Smoothie');
-streamit.text('Hard Boiled Egg');
+streamlit.text('Hard Boiled Egg');
