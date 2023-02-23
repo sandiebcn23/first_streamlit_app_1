b8886c28a60843c819c5f69bdd59d1fa71a188a4
sandiebcn23 <126023504+sandiebcn23@users.noreply.github.com>
2023-02-23 23:40:23 +0100

    test 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
 # my_data_row = my_cur.fetchall()
 # streamlit.text("Hello from Snowflake:")
 streamlit.text("The fruit load list contains: ");
-# streamlit.text(my_data_row)
+# streamlit.text(my_data_row);
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit);
